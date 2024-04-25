8591c20010f2f0d4aa40d17bb4bb906ebb317087
AmanKhys <amankhys2112@gmail.com>
2024-04-26 03:56:41 +0530

    make Create and Update Handler func make GetItemByID func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -8,6 +9,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -20,13 +22,54 @@ const (
 var db, _ = gorm.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
 
 type TodoItemModel struct {
-	Id          int `gorm:"primary key"`
-	Description string
-	Completed   bool
+	Id          int    `gorm:"primary key"`
+	Description string `gorm:"not null"`
+	Completed   bool   `gorm:"not null"`
 }
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new todo Item. Saving to database")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	//	log.Println(todo)
+	db.Create(&todo)
+	//	log.Println(todo)
+	result := db.Last(&todo)
+	//	log.Println(todo)
+	//	log.Println(result.Value)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
 
+}
+
+func GetItemByID(id int) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, id)
+	if result.Error != nil {
+		log.Warn("TodoItem not found in the database")
+		return false
+	}
+	return true
+}
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	completed, _ := strconv.ParseBool(r.FormValue("completed"))
+	err := GetItemByID(id)
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated":false, "error":"Record not found"}`)
+	} else {
+		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
+		todo := &TodoItemModel{Id: id}
+		db.First(&todo)
+		todo.Completed = completed
+		db.Save(&todo)
+
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated":true}`)
+	}
 }
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
@@ -49,5 +92,6 @@ func main() {
 
 	router.HandleFunc("/Healthz", Healthz).Methods("GET")
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", UpdateItem).Methods("PUT")
 	http.ListenAndServe(":8000", router)
 }
